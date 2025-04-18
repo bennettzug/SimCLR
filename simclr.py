119278af2f76ed7138d0c3a6182ec8b28802a2b3
Bennett Zug <bennettzug@gmail.com>
2025-04-18 01:07:56 -0400

    fixed code for simclr

--- simclr.py
+++ simclr.py
@@ -155,7 +155,7 @@ class SimCLR(object):
                 logging.info(f"Unlabeled embeddings saved to {embedding_path}")
 
                 # labeled
-                labeled_embeddings, labeled_labels = self.extract_embeddings(labeled_loader)
+                labeled_embeddings, labeled_labels = self.extract_labelled_embeddings(labeled_loader)
                 labeled_embedding_path = os.path.join(
                     self.embeddings_dir, f"labeled_embeddings_epoch_{epoch_counter + 1}.npz"
                 )
